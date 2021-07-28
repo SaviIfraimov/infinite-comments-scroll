1cb70a90cef7e0d20d2da090f377886f7e4c7306
SaviIfraimov <38398448+SaviIfraimov@users.noreply.github.com>
2021-07-29 02:44:19 +0300

    comments impl, post impl, styles & finishes

--- src/CommentsContainer.jsx
+++ src/CommentsContainer.jsx
@@ -1,91 +1,96 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import './CommentsContainer.css'
 
-const CommentsContainer = () => {
-  // JSONPlaceholder API -  Fetching comments with a certain postId retreives 5 comments.
-  // Randomize a num from 1 to 100 and start reading the comments based on it - it will be the postId
-  const randomInt = Math.floor(Math.random() * 99) + 1;
-  
-  const [loader, setLoader] = useState(true);
-  const [comments, setComments] = useState([]);
-  
-  // Scrolling state
-  const [scrollTop, setScrollTop] = useState(document.body.scrollTop);
-    
-  useEffect(() => {
-    // Get 20 more comments
-    getComments()
+import InfiniteScroll from "react-infinite-scroll-component";
 
-    // Subscribe to scroll event
-    window.addEventListener("scroll", handleOnScroll());
-  }, []);
-  
-  const getComments = () => {
+const CommentsContainer = () => {
+  const [comments, setComments] = useState([]);
+  const [currPostId, setCurrPostId] = useState(1);
+
+  const fetchMoreData = () => {
     try {
+      if (!currPostId) {
+        return;
+      }
+
       let allItems = [];
 
       Promise.all([
-            fetch(`http://jsonplaceholder.typicode.com/comments?postId=${ (randomInt + 0) <= 100 ? (randomInt + 0) : (randomInt + 0 - 100) }`),
-            fetch(`http://jsonplaceholder.typicode.com/comments?postId=${ (randomInt + 1) <= 100 ? (randomInt + 1) : (randomInt + 1 - 100) }`),
-            fetch(`http://jsonplaceholder.typicode.com/comments?postId=${ (randomInt + 2) <= 100 ? (randomInt + 2) : (randomInt + 2 - 100) }`),
-            fetch(`http://jsonplaceholder.typicode.com/comments?postId=${ (randomInt + 3) <= 100 ? (randomInt + 3) : (randomInt + 3 - 100) }`),
+        fetch(
+          `http://jsonplaceholder.typicode.com/comments?postId=${
+            currPostId + 0 <= 100 ? currPostId + 0 : currPostId + 0 - 100
+          }`
+        ),
+        fetch(
+          `http://jsonplaceholder.typicode.com/comments?postId=${
+            currPostId + 1 <= 100 ? currPostId + 1 : currPostId + 1 - 100
+          }`
+        ),
+        fetch(
+          `http://jsonplaceholder.typicode.com/comments?postId=${
+            currPostId + 2 <= 100 ? currPostId + 2 : currPostId + 2 - 100
+          }`
+        ),
+        fetch(
+          `http://jsonplaceholder.typicode.com/comments?postId=${
+            currPostId + 3 <= 100 ? currPostId + 3 : currPostId + 3 - 100
+          }`
+        ),
       ]).then((responses) => {
-          Promise.all([
-            responses[0].json(),
-            responses[1].json(),
-            responses[2].json(),
-            responses[3].json()
-          ]).then((commentsBatchesArray) => {
-              if (commentsBatchesArray.length > 0) {
-                commentsBatchesArray.forEach(item => {
-                  allItems = allItems.concat(item);
-                });
+        Promise.all([
+          responses[0].json(),
+          responses[1].json(),
+          responses[2].json(),
+          responses[3].json(),
+        ]).then((commentsBatchesArray) => {
+          if (commentsBatchesArray.length > 0) {
+            commentsBatchesArray.forEach((item) => {
+              allItems = allItems.concat(item);
+            });
 
-                setComments(allItems);
-                setLoader(false);
-              }
-          })
-      }) 
-    } catch(error) {
+            setComments([...new Set([...comments, ...allItems])]);
+            setCurrPostId(currPostId !== 97 ? currPostId + 4 : 0);
+          }
+        });
+      });
+    } catch (error) {
       // Catch error the way you decide.
     }
-  }
+  };
 
-  //TODO continue here
-  ...
-  const handleOnScroll = () => {
-    // Check to see if scrolling near bottom of page, load more comments
-    if (
-      window.innerHeight + window.scrollY >=
-      document.body.offsetHeight - 1000
-      ) {
-      console.log(window.innerHeight)
-      console.log(document.body.offsetHeight)
-      getComments();
-    }
-  } 
+  // Initial fetching of data
+  if (currPostId === 1 && !comments.length) fetchMoreData();
 
   return (
     <div>
       <div className="comments-container" id="comments-container">
-
-        {loader ? (
-          <div className="loader" id="loader"></div>
-        ) : (
-          <>
-            {comments.map(function(comment, index) {
+        <InfiniteScroll
+          dataLength={comments.length}
+          next={fetchMoreData}
+          hasMore={true}
+          loader={(currPostId === 1 && !comments.length) ? <div className="loader" id="loader"></div> : ""}
+          scrollThreshold={0.85}
+          endMessage={
+              <b style={{textAlign: 'center'}}>End of Results</b>
+            }
+        >
+          <div>
+            {comments.map((comment, index) => {
               return (
                 <div className="comment" key={index}>
-                  <br/><span> {comment.email.split("@")[0]} </span>
-                  <p> 
-                  <br/><br/>
-                  {comment.body}
-                  </p>  
-                </div> )
+                  <br />
+                  <span> {comment.email.split("@")[0]} </span>
+                  <p>
+                    <br />
+                    <br />
+                    {comment.body}
+                  </p>
+                </div>
+              );
             })}
-            <br/>
-          </>
-        )}
+            <br />
+          </div>
+        </InfiniteScroll>
       </div>
     </div>
   );
--- src/PostComment.jsx
+++ src/PostComment.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './PostComment.css'
 
 const PostComment = () => {
+    const [inputText, setInputText] = useState("")
+
     return (
         <div>
             <form>
-                <input className="input-text" type="text" placeholder="Write Your Comment..." required/>
-                <input className="input-submit" type="submit" value="POST COMMENT"/>
+                <input className="input-text" type="text" placeholder="Write Your Comment..." onChange={(e) => setInputText(e.target.value)} required/>
+                <input className="input-submit" type="submit" value="POST COMMENT" onClick={(e) => { e.preventDefault(); if (inputText !== "") sendCommentPostRequest(inputText) }}/>
             </form>
         </div>
     )
+}
+
+const sendCommentPostRequest = (value) => {
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({
+            postId: 0,
+            name: 'the one who posts comments',
+            body: value,
+            email: "abc000@jsonplaceholderapi.com",
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    };
+    
+    fetch('http://test.steps.me/test/testAssignComment', requestOptions)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => {
+            console.error('There was an error!', error);
+        });
 }
 
 export default PostComment
